a1531697df618639dea5eab130bb27a8dfeafc5c
wmalott23 <101433624+wmalott23@users.noreply.github.com>
2022-04-28 08:14:29 -0700

    Got like button to work against the database

--- src/Components/Song/Song.jsx
+++ src/Components/Song/Song.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { AiOutlineHeart } from 'react-icons/ai'
+import axios from 'axios';
+import React, { useState } from 'react';
+import { BsFillBookmarkHeartFill } from 'react-icons/bs';
 import './Song.css'
 
 const Song = ({id, title, artist, album, release_date, genre, liked, image_url, num_likes}) => {
 
+    //background image based on album cover
     var divStyle = {
         backgroundImage: `url(${image_url})`,
         height: `150px`,
@@ -13,10 +15,37 @@ const Song = ({id, title, artist, album, release_date, genre, liked, image_url, 
         backgroundSize: `cover`,
     }
 
+    //like button
+
+    const [like, setLike] = useState(0)
+
+    const likedURL = `http://127.0.0.1:8000/api/music_library/liked/`;
+    const dislikedURL = `http://127.0.0.1:8000/api/music_library/disliked/`;
+
+    let likedSong = {
+        liked: "True",
+    }
+    let dislikedSong = {
+        liked: "False",
+    }
+
+    async function handleClick (){ 
+        if(like === 0) {
+            await axios.patch(`${likedURL}${id}/`, likedSong);
+            setLike(1)
+        }
+        if(like === 1) {
+            await axios.patch(`${dislikedURL}${id}/`, dislikedSong);
+            setLike(0)
+        }
+    }
+
+
     return ( 
         <tr className="all-rows" style={divStyle}>
-            <th className="">
-                <h6 className="entry h2">{num_likes}<AiOutlineHeart/></h6>
+            <th className="entry h4">
+                {num_likes}
+                <BsFillBookmarkHeartFill href="" className="btn" size={50} onClick={handleClick}/>
             </th>
             <th className="entry h4 h-25">{id}</th>
             <th className="entry h4">{title}</th>
